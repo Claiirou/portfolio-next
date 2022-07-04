5e76b1ddda845ac8e210d9eb22fd906bdc051959
Claiirou <clairemachet@hotmail.fr>
2022-07-04 23:03:41 +0200

    add navbar menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { BsPersonLinesFill } from "react-icons/bs";
 
 const Navbar = () => {
   return (
     <div className="fixed  w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <Image
-          src="/../assets/images/Photos.png"
+          src="/../public/assets/images/photo.png"
           alt="photo"
-          width="50"
-          height="100"
+          width="60"
+          height="80"
         />
         <div>
           <ul className="hidden md:flex">
@@ -25,7 +28,7 @@ const Navbar = () => {
             </Link>
             <Link href="/">
               <li className="ml-10 text-sm uppercase hover:border-b">
-                Project
+                Projects
               </li>
             </Link>
             <Link href="/">
@@ -34,7 +37,70 @@ const Navbar = () => {
               </li>
             </Link>
           </ul>
-          <div></div>
+          <div className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+          <div className="">
+            <div className="flex w-full items-center justify-between">
+              <Image
+                src="/../public/assets/images/photo.png"
+                alt="/"
+                width="87"
+                height="35"
+              />
+              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-4">
+              <p className="w-[85%] md:w-[90%] py-4">
+                Lets build something legendary together
+              </p>
+            </div>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <Link href="/">
+                <li className="py-4 text-sm">Home</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">About</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Skills</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Projects</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Contact</li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest text-[#5651e5]">
+                Lets connect
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaLinkedinIn />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaGithub />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <AiOutlineMail />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsPersonLinesFill />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
